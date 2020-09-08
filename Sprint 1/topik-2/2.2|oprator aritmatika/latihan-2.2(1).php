09474291246964ce757498b6c5cbbf84462aeef0
FaRaJiAs99 <fadhillajihad99@gmail.com>
2020-09-08 10:15:05 +0700

    latihan2

---
+++ Sprint 1/topik-2/2.2|oprator aritmatika/latihan-2.2(1).php
@@ -0,0 +1,10 @@
+<?php
+
+$Panjang_persegi= 5;
+$Lebar_persegi= 8;
+
+$hasil_perhitungan = $Panjang_persegi*$Lebar_persegi;
+
+echo "Panjang: " . $Panjang_persegi . "</br>";
+echo "Lebar: " . $Lebar_persegi . "</br>";
+echo "Luas persegi tersebut adalah: " . $hasil_perhitungan;
